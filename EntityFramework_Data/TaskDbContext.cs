b56368b9083d8d97d0e8f201e46a6f81353da795
Gagan Kumar <72305065+Robox-Asus@users.noreply.github.com>
2025-05-29 01:25:58 +0530

    Integrate EF Core
    Optimize and Add Entity Framework

---
+++ EntityFramework_Data/TaskDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager_MinimalAPI.Models;
+
+namespace EntityFramework_Data
+{
+    public class TaskDbContext(DbContextOptions<TaskDbContext> options) : DbContext(options)
+    {
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
